38dc8ed5be9ca931189386f799962c7424c68def
ShabbirAli00c <159994827+ShabbirAli00c@users.noreply.github.com>
2024-02-21 12:04:06 +0500

    Add files via upload

---
+++ 12 Names.ts
@@ -0,0 +1,8 @@
+
+let names :string[] = ['Shabbir', 'Ismail', 'Bilal', 'Ali'];
+
+const message :string = 'Welcome to Typescript class: '
+
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]);
+}
