773bd62261479742254c69ccc79b539384936aba
ShabbirAli00c <159994827+ShabbirAli00c@users.noreply.github.com>
2024-03-21 10:40:26 +0500

    Add files via upload

---
+++ 23.conditional_tests.js
@@ -0,0 +1,17 @@
+"use strict";
+//Write a series of conditional tests.
+Object.defineProperty(exports, "__esModule", { value: true });
+//Creating car variable
+let car = 'subaru';
+//Test No 1
+console.log("Is car ==='subaru'? I predict True.");
+console.log(car === 'subaru');
+//Test No 2
+console.log("Is car =='subaru'? I predict True.");
+console.log(car == 'subaru');
+//Test No 3
+console.log("Is car !='Honda'? I predict True.");
+console.log(car != 'Honda');
+//Test No 4
+console.log("Is car !=='ford'? I predict True.");
+console.log(car !== 'ford');
